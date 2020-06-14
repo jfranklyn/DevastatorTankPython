da16ecd1c900d2e101b9a42dc8b2bf103addd72a
jfranklyn <59396137+jfranklyn@users.noreply.github.com>
2020-06-14 13:58:40 -0500

    updated keyboard codes

--- DevstatorTankPython.py
+++ DevstatorTankPython.py
@@ -1,16 +1,42 @@
 #!/usr/bin/python
 from evdev import InputDevice , categorize, ecodes
 from select import select
+import time
+import sys
+
 # keyboard events only
 dev = InputDevice('/dev/input/event3') 
 print(dev)
 
 import Robot
-from HCSR04Sensor import distance
+#from HCSR04Sensor import distance
 #from neopixel_rpi_python import *
 
+from Raspi_PWM_Servo_Driver import PWM
+
+# ===========================================================================
+# Servo Code
+# ===========================================================================
+
+# Initialise the PWM device using the default address
+# bmp = PWM(0x40, debug=True)
+pwm = PWM(0x6F)
+servoMin = 150  # Min pulse length out of 4096
+servoMax = 600  # Max pulse length out of 4096
+pwm.setPWMFreq(60)   # Set frequency to 60 Hz
+
+def setServoPulse(channel, pulse):
+  pulseLength = 1000000                   # 1,000,000 us per second
+  pulseLength /= 60                       # 60 Hz
+  print ("%d us per period" % pulseLength)
+  pulseLength /= 4096                     # 12 bits of resolution
+  print ("%d us per bit" % pulseLength)
+  pulse *= 1000
+  pulse /= pulseLength
+  pwm.setPWM(channel, 0, pulse)
+
 # Set the trim offset for each motor (left and right).    
-LEFT_TRIM   = -1
+LEFT_TRIM   = 0
 RIGHT_TRIM  = 0
 
 # Create an instance of the robot with the specified trim values.
@@ -21,7 +47,7 @@ RIGHT_TRIM  = 0
 robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)    
 
 def check_distance():
-# check distance b4 moving    
+# check distance b4 moving. NOT USED
     curDis = distance()
     print('Current distance in CM: ', curDis)
     if distance() < 15:
@@ -44,26 +70,34 @@ def check_distance():
 def key_input(event):
     #print ('Key:', event.value, event.code)
     #sleep_time = 0.030
+#   UP arrow key    
     if (event.code == 103 and event.value == 0):
-        check_distance()
+        #check_distance()
         print ("Forward")
         robot.forward(150)
         sleep_time = 0.030
     elif (event.code == 108 and event.value == 0):
-        check_distance()
+        #check_distance()
         print  ("Back")
         robot.backward(150)
         sleep_time = 0.030
     elif (event.code == 106 and event.value == 0):
-        check_distance()
+        #check_distance()
         print ("right")
         robot.right(100, 0.15)
         sleep_time = 0.030
     elif (event.code == 105  and event.value == 0):
-        check_distance()
+        #check_distance()
         print ("left")
         robot.left(100, 0.15)
         sleep_time = 0.030
+#   press S key to control servo        
+    elif (event.code == 115  and event.value == 0):
+  # Change speed of continuous servo on channel O
+        pwm.setPWM(0, 0, servoMin)
+        time.sleep(0.5)
+        pwm.setPWM(0, 0, servoMax)
+        time.sleep(0.5)        
 # enter = stop        
     elif event.code == 28:
         print ("stop")
@@ -75,7 +109,7 @@ try:
         for event in dev.read_loop():
 # start LED's blinking. sleep for 5 ms after each cycle           
             #rainbow_cycle(0.005)
-            check_distance()
+            #check_distance()
             key_input(event)
             
 except KeyboardInterrupt:
